a105ab73f63c3d256a96d8009c39261b39de461b
Emily Lu <eclu2014@gmail.com>
2020-01-09 16:24:15 -0500

    create filter and sort components

--- public/src/frontend/MapPage.js
+++ public/src/frontend/MapPage.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import GoogleMap from './components/GoogleMap'
+import Sort from './components/Sort'
+import Filter from './components/Filter'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
+// onClick = e => {
+//     let checkList = e.target;
+
+// }
+const drop = {
+    textAlign: 'center',
+    display: 'inline-block'
+}
+
 const MapPage = () => (
-    <div style={{textAlign:'center', display:'inline-block'}}> 
-        <GoogleMap />
+    <div style={drop}> 
+        <div><Sort /></div>
+        <div><Filter /></div>
+        <div style={{textAlign:'right'}}>
+            <GoogleMap />
+        </div>
     </div>
 )
 
