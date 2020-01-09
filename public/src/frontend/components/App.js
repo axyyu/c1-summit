17ec1d97d9b0da782049db6cc678cc03fe228cd3
Emily Lu <eclu2014@gmail.com>
2020-01-09 15:30:55 -0500

    add map onto page

--- public/src/frontend/components/App.js
+++ public/src/frontend/components/App.js
@@ -2,17 +2,26 @@ import React from 'react'
 import '../../styles/styles.css'
 import Search from './search.js'
 import Suggestion from './Suggestion.js'
+import GoogleMap from './GoogleMap.js'
 
 const searchBar = {
   marginTop: '100px',
   marginBottom: '100px'
 };
 
+const map = {
+  display: 'inline-block',
+  marginRight: '40px'
+}
+
 function App() {
     return (
       <div style={{ textAlign:"center" }}>
          <Search style={{searchBar}} placeholder='Who are you going with?'/>
          <Search style={{searchBar}} placeholder='What are you feeling?' />
+         <div style={{margin:'auto'}}>
+          <GoogleMap />
+         </div>
       </div>  
     );
 }
