ffb8c63ebb96df318608d49c179052b9030ad98f
Emily Lu <eclu2014@gmail.com>
2020-01-09 13:52:38 -0500

    add search bar

--- public/src/frontend/components/App.js
+++ public/src/frontend/components/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import '../../styles/styles.css'
+import Search from './search.js';
+
+const searchBar = {
+  marginTop: '100px',
+  marginBottom: '100px'
+};
 
 function App() {
-  return <div>hello</div>
+    return (
+      <div style={{ textAlign:"center" }}>
+         <Search style={{searchBar}} placeholder='Who are you going with?'/>
+         <Search style={{searchBar}} placeholder='What are you feeling?' />
+      </div>  
+    );
 }
 
 export default App
