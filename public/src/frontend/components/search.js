8909821ed9a2cf63313f7c287f03dbb551bab5cc
Emily Lu <eclu2014@gmail.com>
2020-01-09 13:29:20 -0500

    add search.js

---
+++ public/src/frontend/components/search.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.js";
+
+export default class Search extends React.Component {
+    state = {
+        
+    }
+    
+
+    change = e => {
+		this.setState({[e.target.name]: e.target.value});
+    };
+    
+    render() {
+        return (
+            <form>
+                <input type="text" name="search" placeholder={this.state.placeholder}></input>
+            </form>
+        );
+    }
+}
