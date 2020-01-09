c6a58a98e0d26931a11565d5d8e9e5535f40884b
Emily Lu <eclu2014@gmail.com>
2020-01-09 15:11:49 -0500

    added maps?

--- public/src/frontend/components/GoogleMap.js
+++ public/src/frontend/components/GoogleMap.js
@@ -1,26 +1,56 @@
 import React from 'react'
 import './App.js'
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-class GoogleApiWrapper extends React.Component {
-    state = {}
+class GoogleMap extends React.Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          restaurant: [],
+          friends: []
+        }
+    }
+
+    displayFriends = () => {
+      return this.state.friends.map((person, index) => {
+        return <Marker key={index} id={index} position={{
+         lat: person.latitude,
+         lng: person.longitude
+       }}
+       onClick={() => console.log("You clicked me!")} /> //display which friend once clicked
+      })
+    }
+
+    displayRestaurant = () => {
+        return <Marker key={index} id={index} position={{
+           lat: restaurant.latitude,
+           lng: restaurant.longitude
+         }}
+         onClick={() => console.log("You clicked me!")} /> //replace with some function
+      }
 
     render() {
+        const mapStyle = {
+            width: '40%',
+            height: '40%'
+        };
+
         return (
             <div>
                  <Map
+                    style={mapStyle}
                     google={this.props.google}
                     zoom={8}
                     style={mapStyles}
                     initialCenter={{ lat: 47.444, lng: -122.176 }} //change default center to first suggested location 
                 />
+                {this.displayFriends()}
+                {this.displayRestaurant()}
             </div>
         );
     }
 }
 
-//export default GoogleMap
-
-export default GoogleApiWrapper({
-    apiKey: 'TOKEN HERE'
-})(MapContainer);
+export default GoogleMap
