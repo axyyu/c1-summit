586688351176c8d93660841a4ab51170c4cf00ba
Andrew <andrewxiaoyu0@gmail.com>
2020-01-10 01:50:48 -0500

    Add nn to location search

--- server/optimalLocation.py
+++ server/optimalLocation.py
@@ -6,6 +6,7 @@ from makePrediction import make_prediction
 from googleMaps import search_cuisineAll
 from felixfunctions import get_avg_spending_v2, final_area
 from firebaseClient import FirebaseClient
+from neuralNet import machineLearning
 
 def get_cost(price):
   return price/10
@@ -18,19 +19,22 @@ def find_optimal_location(user_ids):
     if doc.id in user_ids:
       users.append(doc.to_dict())
 
-  top_categories = make_prediction(users)
-  price = get_avg_spending_v2(users)
-
-  tot_locs = []
+  trans = []
   for doc in fb.db.collection('users').stream():
     if doc.id in user_ids:
       for t in fb.db.collection('users').document(doc.id).collection('transactions').stream():
-        tot_locs.append(t.to_dict()["coordinates"])
+        trans.append(t.to_dict())
 
-  _, center, radius = final_area(tot_locs)
+  price = get_avg_spending_v2(users)
+
+  top_categories = make_prediction(users)
+  recommendations = machineLearning(users, trans, top_categories, price)
+
+  _, center, radius = final_area(trans)
 
   cost = get_cost(price)
 
-  return search_cuisineAll(cost, top_categories, center, radius)
+  return search_cuisineAll(cost, recommendations, center, radius)
 
 # places = find_optimal_location(["4Y6QaMGYMreONwXVM04t", "4tFUneEDRGBBEUsxxKLR"])
+# print(places)
