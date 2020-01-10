8bdfee55f0af9bc302636ea3bcf1aebc69afc8ff
cx-lu <christina.lu.12@gmail.com>
2020-01-09 19:44:31 -0500

    .

--- public/src/frontend/MapPage.js
+++ public/src/frontend/MapPage.js
@@ -7,23 +7,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 const drop = {
-    textAlign: 'center',
-    display: 'inline-block',
+  textAlign: 'center',
+  display: 'inline-block'
 }
 
-const sort = ['Rating','Distance', 'Category', 'Price']
+const sort = ['Rating', 'Distance', 'Category', 'Price']
 const filter = ['Rating', 'Distance', 'Category', 'Price', 'Availability']
 
 const MapPage = () => (
-    <div style={drop}> 
-        <button style={{ float: 'right', width: '100px' }}>
-            &nbsp;&nbsp;&nbsp;&nbsp; Back
-            <FontAwesomeIcon icon={faChevronLeft} style={{ float: 'left' }} />
-        </button>
-            <Drop title='Sort By' list={sort}/>
-            <Drop title='Filter' list={filter}/>
-            <GoogleMap style={{margin:'auto'}}/>
-    </div>
+  <div style={drop}>
+    <button style={{ float: 'right', width: '100px' }}>
+      &nbsp;&nbsp;&nbsp;&nbsp; Back
+      <FontAwesomeIcon icon={faChevronLeft} style={{ float: 'left' }} />
+    </button>
+    <Drop title='Sort By' list={sort} />
+    <Drop title='Filter' list={filter} />
+    <GoogleMap style={{ margin: 'auto' }} />
+  </div>
 )
 
 export default MapPage
